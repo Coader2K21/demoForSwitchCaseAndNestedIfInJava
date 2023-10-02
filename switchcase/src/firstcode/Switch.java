4bad8438ea898d8ef631d44416f294afc6ec6585
Coader2K21 <146635215+Coader2K21@users.noreply.github.com>
2023-10-02 18:15:51 +0530

    Add files via upload

---
+++ switchcase/src/firstcode/Switch.java
@@ -0,0 +1,21 @@
+package firstcode;
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		int value=1000;
+		
+		switch(value) {
+		default:
+			System.out.println("no salary bro");
+			break;
+		case 1000:
+			System.out.println("you got a good salary");
+			break;
+		case 500:
+			System.out.println("you got a bad salary");
+			break;
+			
+		}
+	}
+}
